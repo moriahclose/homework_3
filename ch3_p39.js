13196246255d8ab6e7c46ba6b533012227bb5762
mtollive <mtollive@lion.lmu.edu>
2017-11-07 18:35:44 -0800

    Update ch3_p39.js

--- ch3_p39.js
+++ ch3_p39.js
@@ -1,24 +1,41 @@
-function ch3(){
 // get hexadecimal from user
 var inputHex = prompt("Enter a number in hexadecimal:");
 
 // create output variable
 var outputDecimal = 0;
 
-// create array of letters in hex
-var hexLetters = ["A","B","C","D","E","F"];
+// create array of characters in hex
+var hexLettersUpper = ["A", "B", "C", "D", "E", "F"];
+var hexLettersLower = ["a", "b", "c", "d", "e", "f"];
+var hexNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-// go through string from back to front
+// make sure input is a hex number
+for (var i = 0; i < inputHex.length; i++) {
+    if (hexLettersUpper.indexOf(inputHex.charAt(i)) == -1 && hexNumbers.indexOf(inputHex.charAt(i)) == -1 && hexLettersLower.indexOf(inputHex.charAt(i)) == -1) {
+        alert("Please enter a hex value.");
+        break;
+    }
+}
+
+// create variable to hold current exponent
 var exponent = 0;
 
-for (var i = inputHex.length-1; i >= 0; i--){
-    if (inputHex.charAt(i) < 0 || inputHex.charAt(i) > 9){
-        var convertThis = 10 + inputHex.indexOf(inputHex.charAt(i));
-    } 
-    var convertThis = parseInt(inputHex.charAt(i));
-    outputDecimal += (convertThis * (16 ** exponent));
+// covert each input character to its hex equivalent
+for (var i = inputHex.length - 1; i >= 0; i--) {
+    if (hexNumbers.indexOf(inputHex.charAt(i)) > -1) {
+        var convertNum = parseInt(inputHex.charAt(i));
+        outputDecimal += convertNum * (16 ** exponent);
+    }
+    else if (hexLettersUpper.indexOf(inputHex.charAt(i)) > -1) {
+        var convertNum = 10 + hexLettersUpper.indexOf(inputHex.charAt(i));
+        outputDecimal += convertNum * (16 ** exponent);
+    }
+    else {
+        var convertNum = 10 + hexLettersLower.indexOf(inputHex.charAt(i));
+        outputDecimal += convertNum * (16 ** exponent);
+    }
     exponent++;
 }
 
-alert( outputDecimal );
-}
+// output the decimal equivalent of the input hex number
+alert(outputDecimal);

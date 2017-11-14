2113c3d316ec8c68fbe494918c46ef9b918dfcba
mtollive <mtollive@lion.lmu.edu>
2017-11-13 21:21:12 -0800

    Update exercise4.py

--- exercise4.py
+++ exercise4.py
@@ -1,8 +1,8 @@
 # exercise4.py
 # Moriah Tolliver and Gabriel Say
 
-balance = int(input("Initial balance: "));
-monthlyInterestRate = int(input("Annual interest rate in percent: ")) / 1200;
+balance = float(input("Initial balance: "));
+monthlyInterestRate = float(input("Annual interest rate in percent: ")) / 1200;
 
 for balanceNum in range (1 , 4):
      balance = balance + ( balance * monthlyInterestRate )

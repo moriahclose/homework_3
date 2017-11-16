4b22e09d4fd33675662079e6cb37f2f621cf7c27
gabrielsay <gsay617@gmail.com>
2017-11-16 02:14:26 -0800

    Add files via upload

---
+++ Quadratic Formula Solution.js
@@ -0,0 +1,24 @@
+var a, b, c;
+var outputText;
+
+function validate() {
+
+a = document.forms["input_form"]["ainput"].value;
+b = document.forms["input_form"]["binput"].value;
+c = document.forms["input_form"]["cinput"].value;
+
+if (a == 0) {
+   outputText = "Can't equal zero!";
+} else if (isNaN(a)) {
+   outputText = "Must be a number.";
+} else if (isNaN(b)) {
+   outputText = "Must be a number.";
+} else if (isNaN(c)) {
+   outputText = "Must be a number.";
+} else  {
+    var x1 = (-b - Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
+    var x2 = (-b + Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
+    outputText = a + "x\u00B2 +" + b + "x + " + c + " = 0, x can equal " + x1 + " or " + x2;
+ }
+   document.getElementById("output_text").innerHTML = outputText;
+}
